d2569fa871a7142328305433612924469337a8ad
AnahuacCRM <31287806+AnahuacCRM@users.noreply.github.com>
2017-08-29 11:33:24 -0500

    Initial commit

--- Baner.Recepcion/Baner.Recepcion.Services/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Baner.Recepcion.Services.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- Baner.Recepcion/Baner.Recepcion.Services/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Baner.Recepcion.Services.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
