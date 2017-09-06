6cbc8aaf5cc71df3eecb3e7f613529c044e8b0ab
gucane <31449790+gucane@users.noreply.github.com>
2017-09-06 12:04:32 -0600

    3917
    Servicio para el monitoreo de url,
    saber si hay conexion con CRM e integraciones

--- Baner.Recepcion/Baner.Recepcion.Services/Controllers/srvStatusConexionCRMController.cs
+++ Baner.Recepcion/Baner.Recepcion.Services/Controllers/srvStatusConexionCRMController.cs
@@ -4,6 +4,7 @@ using Baner.Recepcion.DataInterfaces;
 using Baner.Recepcion.OperationalManagement;
 using Baner.Recepcion.OperationalManagement.Exceptions;
 using Baner.Recepcion.OperationalManagement.Extensions;
+using Microsoft.Xrm.Sdk;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,55 +16,35 @@ namespace Baner.Recepcion.Services.Controllers
 {
     public class srvStatusConexionCRMController : ApiController
     {
-        private readonly ILogger _logger;
-        private readonly IMonitoreoConexion _MonitoreoConexion;
-        public srvStatusConexionCRMController(ILogger logger, IMonitoreoConexion monitoreoCrm)
-        {
-            this._logger = logger;
-            this._MonitoreoConexion = monitoreoCrm;
-        }
+        
 
         [HttpPost]
         public HttpResponseMessage StatusCrmConexion()
         {
             try
             {
-                bool res = _MonitoreoConexion.NotificaConexionCRM();
+                string org = System.Configuration.ConfigurationManager.AppSettings["uri"];
+                string user = System.Configuration.ConfigurationManager.AppSettings["username"];
+                string pass = System.Configuration.ConfigurationManager.AppSettings["password"];
 
-                if (res)
-                    return Request.CreateResponse<bool>(HttpStatusCode.OK, _MonitoreoConexion.NotificaConexionCRM());
-                else
-                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "ocurrió un problema al intentar establecer conexión con Dynamics CRM");
+                CRM365.Conector.Service service = new CRM365.Conector.Service(org, "", user, pass);
+                IOrganizationService _xrmServerConnection2;
+
+                _xrmServerConnection2 = service.OrganizationService;
+                return Request.CreateResponse<bool>(HttpStatusCode.OK, true);
+
+
+                //return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "surgio este erro al intentar conectarce al crm  " + ex.Message);
+
 
             }
-            catch (BusinessLayerValidationException ex)
-            {
-                _logger.Error("BusinessLayerValidationException :" + ex.Message);
-                string errores = "";
-                foreach (var e in ex.Errors)
-                {
-                    errores += string.Format("{0}\n ", e);
-                }
-                _logger.Error("BusinessLayerValidationException Errores : " + errores);
 
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, errores, ex);
-            }
-            catch (PickListException ex)
-            {
-                _logger.Error("PickListException  " + ex.Message);
 
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Errors);
-            }
-            catch (LookupException ex)
-            {
-                _logger.Error("LookupException  " + ex.Message);
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Errors);
-            }
             catch (Exception ex)
             {
-                _logger.Error("Exception   " + ex.Message);
+                // _logger.Error("Exception   " + ex.Message);
                 //_logger.Error(ex);
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Build());
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Ocurrio un error al intentar conectarce con crm: " + ex.Message);
             }
         }
     }
--- Baner.Recepcion/Banner.Recepcion.WebApiTesting/WebApiStatusCrmConexion.cs
+++ Baner.Recepcion/Banner.Recepcion.WebApiTesting/WebApiStatusCrmConexion.cs
@@ -17,25 +17,12 @@ namespace Banner.Recepcion.WebApiTesting
     public class WebApiStatusCrmConexion
     {
 
-        ILogger _logger;
-        IMonitoreoConexion _MonitoreoConexion;
-        IMonitoreoProcessor _MonitoreoProcessor;
-       
         srvStatusConexionCRMController _SrvStatusCrm;
 
         [TestInitialize]
         public void Initializate()
         {
-            _logger = new DebugerLogger();
-            //_catalogrepository = new CatalogRepository();
-            //_picklistrepository = new PickListRepository();
-            //opportunityRepository = new OpportunityRepository();
-            //_prospectRepository = new ProspectRepository(_catalogrepository, _picklistrepository, opportunityRepository);
-            _MonitoreoConexion = new MonitorearConexionCrm();
-            //_prospectprocessor = new ProspectProcessor(_logger, _prospectRepository);
-            _MonitoreoProcessor = new MonitoreoProcessor(_logger, _MonitoreoConexion);
-            //_servCambioExaminado = new srvCambioExaminadoController(_logger, _prospectprocessor);
-            _SrvStatusCrm = new srvStatusConexionCRMController(_logger, _MonitoreoConexion);
+            _SrvStatusCrm = new srvStatusConexionCRMController();
             _SrvStatusCrm.Request = new HttpRequestMessage()
             {
                 Properties = { { HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration() } }
@@ -50,7 +37,7 @@ namespace Banner.Recepcion.WebApiTesting
             if (resultado.StatusCode != HttpStatusCode.OK)
             {
                 var msgresultado = resultado.Content.ReadAsStringAsync().Result;
-                _logger.Infomacion(msgresultado);
+               
             }
 
         }
